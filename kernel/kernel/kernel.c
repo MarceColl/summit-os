87cbf870bf9e2e9a36297f02a54ca10892a07e7e
Marcelino Coll Rovira <coll.marce@gmail.com>
2016-11-18 00:27:40 +0100

    things

--- kernel/kernel/kernel.c
+++ kernel/kernel/kernel.c
@@ -2,11 +2,18 @@
 
 #include <kernel/tty.h>
 
+
+void print_banner() {
+	printf("  ____                            _ _      ___  ____  \n");
+	printf(" / ___| _   _ _ __ ___  _ __ ___ (_) |_   / _ \\/ ___| \n");
+	printf(" \\___ \\| | | | '_ ` _ \\| '_ ` _ \\| | __| | | | \\___ \\ \n");
+	printf("  ___) | |_| | | | | | | | | | | | | |_  | |_| |___) |\n");
+	printf(" |____/ \\__,_|_| |_| |_|_| |_| |_|_|\\__|  \\___/|____/  v0.0.1\n\n");
+	printf("(c) 2016 Marcelino Coll\n");	
+}
+
+
 void kernel_main(void) {
 	terminal_initialize();
-	printf("Hello, kernel World!\n");
-
-	for(int i = 0; i < 200000; ++i) {
-		printf("%d", i);
-	}
+	print_banner();
 }
