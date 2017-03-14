7c136b5c66ed383b5ef6e3801c7bb07075450418
Marcelino Coll Rovira <coll.marce@gmail.com>
2017-03-14 23:26:47 +0100

    Fixed IDT

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ for PROJECT in $PROJECTS; do
   (cd $PROJECT && DESTDIR="$SYSROOT" $MAKE install)
 done
 
+mkdir -p isodir/boot
+
 cp kernel/summit.kernel isodir/boot/
 grub-mkrescue -d /usr/lib/grub/i386-pc -o summit.iso isodir
-bochs -q
